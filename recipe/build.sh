4b2afe4daf946844a4ca7d5ec9ff4cacd26a2322
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-08 19:26:06 -0800

    fix cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,6 @@
 
 if [[ ${build_platform} != ${target_platform} ]]; then
     sed -i "s?R=\"\${R_HOME}/bin/R\"?R=\"${BUILD_PREFIX}/bin/R\"?" configure
-    cat configure
 fi
 
 export DISABLE_AUTOBREW=1
