8e4a1e140ff14efc204aba4463eaa3304696e8fa
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-24 10:16:23 -0600

    enable osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+if [[ ${build_platform} != ${target_platform} ]]; then
+    sed -i 's/R_HOME/BUILD_PREFIX/' configure
+fi
+
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
